f30f372c47ebc661e73f63cd240298e1edd35179
Jonah Majumder <jmajumder9@gmail.com>
2018-10-27 17:35:31 -0400

    removing ref to feature 3 which doesn't exist yet

--- definitions.js
+++ definitions.js
@@ -4,7 +4,6 @@
 var pages = [
     {name: "Histograms", link: "index.html"},
     {name: "NFL Win Probabilities", link: "winProbs.html"},
-    {name: "Feature 3", link: "feature3.html"},
 ];
 
 var margin_width = 4;
